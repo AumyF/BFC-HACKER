60d7c1ca167cc9a14e23dea50b1418ab4714a960
Mominis <46275902+MominisJapan@users.noreply.github.com>
2019-05-03 18:46:05 +0900

    v

--- bfc-hacker.js
+++ bfc-hacker.js
@@ -2,7 +2,7 @@
 // @name             BFC-HACKER midnight
 // @name:ja          BFC-HACKER midnight
 // @namespace        https://github.com/mominisjapan
-// @version          2m
+// @version          2
 // @description      a tool to toot "Hacker Emoji" easier for best-friends.chat
 // @description:ja   best-friends.chatのハッカー絵文字を簡単に入力
 // @author           Mominis
@@ -10,7 +10,7 @@
 // @grant            none
 // @run-at           document-end
 // ==/UserScript==
-const version = "2m";
+const version = "2";
 window.onload = () => {
     console.log(`BFC-HACKER ${version}\nBy Mominis(mn@best-friends.chat)`);
     const elm = document.createElement('button');
