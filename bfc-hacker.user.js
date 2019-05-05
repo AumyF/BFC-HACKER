96f3c1d4dfb7cb062388f1bb094f14c77340de25
Mominis/もみにす <46275902+MominisJapan@users.noreply.github.com>
2019-05-05 15:05:11 +0900

    Update bfc-hacker.user.js

--- bfc-hacker.user.js
+++ bfc-hacker.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name             BFC-HACKER
 // @namespace        https://github.com/mominisjapan
-// @version          4
+// @version          5
 // @description      best-friends.chatのハッカー絵文字を簡単に入力
 // @author           Mominis
 // @match            https://best-friends.chat/*
 // @grant            none
 // @run-at           document-end
 // ==/UserScript==
-const version = "4";
+const version = "5";
 window.onload = () => {
     console.log(`BFC-HACKER ${version}\nBy Mominis(mn@best-friends.chat)`);
     const elm = document.createElement('button');
@@ -20,26 +20,44 @@ window.onload = () => {
 }
 const hacking = () => {
     let textarea = document.getElementsByClassName('autosuggest-textarea__textarea')[0]
-    textarea.value = sift(textarea.innerHTML.toLowerCase()).join('');
+    textarea.value = sift(textarea.innerHTML).join('');
 }
 
 let needsSpaceBefore = false;
+let isBetweenSemicolon = false;
 const sift = (str) => {
     needsSpaceBefore = false;
     let hack = [];
     for(let i=0;i!=str.length;i++){
-        if(str.codePointAt(i) >= 0x61 && str.codePointAt(i) <= 0x7a){
+        const charC = str.codePointAt(i);
+        if(charC==0x3b){
+            //semicolon
+            console.log(`BFC-HACKER:No.${i} is semicolon`);
+            if(isBetweenSemicolon){
+                hack[i] = `${space()}`;
+                needsSpaceBefore = false;
+                isBetweenSemicolon = false;
+            }else{
+                hack[i] = ``;
+                needsSpaceBefore = true;
+                isBetweenSemicolon = true;
+            }
+        }else if(isBetweenSemicolon){
+            console.log(`BFC-HACKER:No.${i}(${charC}) is between semicolons`);
+            hack[i] = str[i];
+        }else if(charC >= 0x61 && charC <= 0x7a){
             //latin small, "abcdefghijklmnopqrstuvwxyz"
-            console.log('BFC-HACKER:LATIN SMALL LETTER A~Z, No.'+i);
+            console.log(`BFC-HACKER:No.${i} is LATIN SMALL LETTER a~z`);
             needsSpaceBefore = true
             hack[i] = `${space()}:hacker_${str[i]}:`;
-        }else if(str.codePointAt(i) == 0x20){
+        }else if(charC == 0x20){
             //space, " " => ideographic space, "　" (全角スペース)
-            console.log('BFC-HACKER:SPACE, No.'+i);
+            console.log(`BFC-HACKER:No.${i} is SPACE`);
             needsSpaceBefore = false;
             hack[i] = `　`;
         }else{
-            console.log('BFC-HACKER:Other charactor, No.'+i);
+            //semicolon,latin small,spaceのどれでもない
+            console.log(`BFC-HACKER:No.${i} is other charactor`);
             needsSpaceBefore = true;
             hack[i] = `${space()}${str[i]}`;
         }
